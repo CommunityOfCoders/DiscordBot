a7e580bd2241a0513c82c2c52336f82090543ded
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-03-27 13:18:49 +0530

    final

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const Discord = require("discord.js");
 const fs = require("fs");
 
-require("dotenv").config();
+// Express Server
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 const client = new Discord.Client();
 
@@ -20,33 +28,55 @@ for (const file of commandFiles) {
 }
 
 client.on('ready', async () => {
-    console.log("Bot is ready");
+  channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_NUMBER);
+  console.log("Bot is ready");
 })
 
 client.on('message', (msg) => {
-    // ignore if a bot sends the message
-    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+  // ignore if a bot sends the message
+  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
 
-    args = msg.content.slice(prefix.length).trim().split(/ +/);
-    // ["hello", "Saif"]
-    command = args.shift().toLowerCase();
+  args = msg.content.slice(prefix.length).trim().split(/ +/);
+  // ["hello", "Saif"]
+  command = args.shift().toLowerCase();
 
-    if (!client.commands.get(command)) {
-      return msg.reply("No such command found");
-    }
+  if (!client.commands.get(command)) {
+    return msg.reply("No such command found");
+  }
 
-    command = client.commands.get(command);
+  command = client.commands.get(command);
 
-    try {
-      command.execute(msg, args);
-    } catch (error) {
-      console.log(error);
-      return msg.channel.send("There was an error executing the command");
-    }
+  try {
+    command.execute(msg, args);
+  } catch (error) {
+    console.log(error);
+    return msg.channel.send("There was an error executing the command");
+  }
 
 
-    // if (command === "hello"){
-    //   return msg.reply(`Hello, ${args[0]}`);
-    // }
-    // if (command === )
+  // if (command === "hello"){
+  //   return msg.reply(`Hello, ${args[0]}`);
+  // }
+  // if (command === )
+})
+
+client.on("githubMessage", (body) => {
+  console.log(body);
+  const action = body.action;
+  const issueUrl = body.issue.html_url;
+  if (action === "opened") {
+    const message = `New issue created : ${issueUrl}`;
+    channel.send(message);
+  }
+})
+
+app.post("/github", (req, res) => {
+  // Do something here.
+  client.emit("githubMessage", req.body);
+  return res.status(200).json({ success: true });
+})
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log("Server started on port : ", port);
 })
