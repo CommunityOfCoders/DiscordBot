957a61f32232f38fd5626d88926a7cf748bcf805
Yash Tailor <yashtailor2000@gmail.com>
2021-03-27 10:56:58 +0530

    added setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv').config()
+
+const discord = require('discord.js')
+
+const client = new discord.Client()
+
+client.login(process.env.DISCORD_BOT_TOKEN)
+
+client.on('ready',function(){
+    console.log('The client is running...');
+})
+
+client.on('message',function(message){
+    if(message.author.bot)return;
+    message.reply('Hey! What\'s up!?');
+})
